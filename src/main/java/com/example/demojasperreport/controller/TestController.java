05d788ad447e7cf39b0bb4fb26698171311c9474
phat.nguyen <phat.nguyen@formos.com>
2024-05-24 14:30:17 +0700

    add s3

--- src/main/java/com/example/demojasperreport/controller/TestController.java
+++ src/main/java/com/example/demojasperreport/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.example.demojasperreport.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import com.example.demojasperreport.entity.Employee;
 import com.example.demojasperreport.repository.EmployeeRepository;
 import com.example.demojasperreport.service.ReportService;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
@@ -27,6 +34,10 @@ public class TestController {
     private EmployeeRepository repository;
     @Autowired
     private ReportService service;
+    @Autowired
+    private AmazonS3 s3Client;
+    @Value("${application.bucket.name}")
+    private String bucketName;
 
     @GetMapping("/getEmployees")
     public List<Employee> getEmployees() {
@@ -35,13 +46,20 @@ public class TestController {
     }
 
     @GetMapping("/report/{format}")
-    public HttpEntity<InputStreamResource> generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+    public HttpEntity<byte[]> generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
         String path = service.exportReport(format);
         File file = new File(path);
-    
-        InputStream inputStream = new FileInputStream(file);
-    
-        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        String fileName = System.currentTimeMillis() + "_" + "export.pdf";
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, file));
+        // download from s3
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        byte[] content = null;
+        try {
+            content = IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition", "attachment; filename=" + "export.pdf");
@@ -50,6 +68,6 @@ public class TestController {
                 .headers(headers)
                 .contentLength(file.length())
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(inputStreamResource);
+                .body(content);
     }
 }
